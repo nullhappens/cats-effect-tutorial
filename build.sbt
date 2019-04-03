27d8517cc13aaaacb38353ed747cf85190bacea3
Diego Pineda <nullhappens@users.noreply.github.com>
2019-04-02 21:07:41 -0400

    v1 of copyFile code

--- build.sbt
+++ build.sbt
@@ -1,6 +1,9 @@
+ThisBuild / scalafmtOnCompile := true
+
 name := "cats-effect-tutorial"
 version := "1.0"
 scalaVersion := "2.12.8"
+
 libraryDependencies += "org.typelevel" %% "cats-effect" % "1.0.0" withSources() withJavadoc()
 
 scalacOptions ++= Seq(
--- src/main/scala/com/nullhappens/catseffect/CopyFile.scala
+++ src/main/scala/com/nullhappens/catseffect/CopyFile.scala
@@ -1,46 +1,82 @@
-import cats.effect.{IO, Resource}
+package com.nullhappens.catseffect
+
+import cats.effect._
 import java.io.File
+import cats.effect.concurrent.Semaphore
 import cats.implicits._
 import java.io._
 
-object CopyFile extends App {
-  
-  def copy(origin: File, destination: File): IO[Long] = 
-    inputOutputStreams(origin, destination).use { case (in, out) =>
-      transfer(in, out)
-    }
+object CopyFile extends IOApp {
 
-  def inputStream(f: File): Resource[IO, FileInputStream] =
-    Resource.make {
-      IO(new FileInputStream(f))                         // build
-    } { inStream =>
-      IO(inStream.close()).handleErrorWith(_ => IO.unit) // release
-    }
-
-  def outputStream(f: File): Resource[IO, FileOutputStream] =
-    Resource.make {
-      IO(new FileOutputStream(f))                         // build 
-    } { outStream =>
-      IO(outStream.close()).handleErrorWith(_ => IO.unit) // release
-    }
-
-  def inputOutputStreams(in: File, out: File): Resource[IO, (InputStream, OutputStream)] =
+  def copy(origin: File, destination: File)(
+      implicit concurrent: Concurrent[IO]): IO[Long] =
     for {
-      inStream  <- inputStream(in)
-      outStream <- outputStream(out)
-    } yield (inStream, outStream)
-
-  def transmit(origin: InputStream, destination: OutputStream, buffer: Array[Byte], acc: Long): IO[Long] = 
-    for {
-      amount <- IO(origin.read(buffer, 0, buffer.size))
-      count  <- if (amount > -1) IO(destination.write(buffer, 0, amount)) >> transmit(origin, destination, buffer, acc + amount)
-        else IO.pure(acc)
+      guard <- Semaphore[IO](1)
+      count <- inputOutputStreams(origin, destination, guard).use {
+        case (in, out) =>
+          guard.withPermit(transfer(in, out))
+      }
     } yield count
 
-  def transfer(origin: InputStream, destination: OutputStream): IO[Long] = 
+  def inputStream(f: File,
+                  guard: Semaphore[IO]): Resource[IO, FileInputStream] =
+    Resource.make {
+      IO(new FileInputStream(f))
+    } { inStream =>
+      guard.withPermit {
+        IO(inStream.close()).handleErrorWith(_ => IO.unit)
+      }
+    }
+
+  def outputStream(f: File,
+                   guard: Semaphore[IO]): Resource[IO, FileOutputStream] =
+    Resource.make {
+      IO(new FileOutputStream(f))
+    } { outStream =>
+      guard.withPermit {
+        IO(outStream.close()).handleErrorWith(_ => IO.unit)
+      }
+    }
+
+  def inputOutputStreams(
+      in: File,
+      out: File,
+      guard: Semaphore[IO]): Resource[IO, (InputStream, OutputStream)] =
+    for {
+      inStream <- inputStream(in, guard)
+      outStream <- outputStream(out, guard)
+    } yield (inStream, outStream)
+
+  def transmit(origin: InputStream,
+               destination: OutputStream,
+               buffer: Array[Byte],
+               acc: Long): IO[Long] =
+    for {
+      amount <- IO(origin.read(buffer, 0, buffer.size))
+      count <- if (amount > -1)
+        IO(destination.write(buffer, 0, amount)) >> transmit(origin,
+                                                             destination,
+                                                             buffer,
+                                                             acc + amount)
+      else IO.pure(acc)
+    } yield count
+
+  def transfer(origin: InputStream, destination: OutputStream): IO[Long] =
     for {
       buffer <- IO(new Array[Byte](1024 * 10))
-      total  <- transmit(origin, destination, buffer, 0L)
+      total <- transmit(origin, destination, buffer, 0L)
     } yield total
 
+  override def run(args: List[String]): IO[ExitCode] =
+    for {
+      _ <- if (args.length < 2)
+        IO.raiseError(
+          new IllegalArgumentException("Need origin and destination files"))
+      else IO.unit
+      orig = new File(args(0))
+      dest = new File(args(1))
+      count <- copy(orig, dest)
+      _ <- IO(
+        println(s"$count bytes copied from ${orig.getPath} to ${dest.getPath}"))
+    } yield ExitCode.Success
 }
